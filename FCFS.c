67048141cdcc0a595ed0206cbfe57c8b8f672b6c
dijul2346 <146112003+dijul2346@users.noreply.github.com>
2024-08-05 23:23:55 +0530

    Update FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -1,107 +1,80 @@
 #include<stdio.h>
-struct process
-{
-int arrival_time;
-int burst_time;
-int completion_time;
-int waiting_time;
-int turn_Around_time;
-}p[100],temp;
-int main()
-{
-int i,n,k=0,temp_sum=0,j,l;
-float avg_time,avg_time1;
-printf("Enter the no. of processes = ");
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-    printf("Enter the process %d arrival time = ",i+1);
-    scanf("%d",&p[i].arrival_time);
-    printf("Enter the process %d burst time = ",i+1);
-    scanf("%d",&p[i].burst_time);
-}
-for(i=0;i<n-1;i++)
-{
-    for(j=0;j<n-i-1;j++)
-    {
-        if(p[j].arrival_time>p[j+1].arrival_time)
-        {
-            temp = p[j];
-            p[j]=p[j+1];
-            p[j+1]=temp;
+
+struct process{
+    int pid,at,bt,ct,tat,wt;
+}p[20],temp;
+
+void main(){
+    printf("Enter the number of process : ");
+    int n=0,ct=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        p[i].pid=i+1;
+        printf("Enter the arrival time: ");
+        scanf("%d", &p[i].at);
+        printf("Enter the burst time: ");
+        scanf("%d", &p[i].bt);
+
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1-i;j++){
+            if(p[j].at>p[j+1].at){
+                temp=p[j];
+                p[j]=p[j+1];
+                p[j+1]=temp;
+            }
         }
     }
-}
-for(i=0;i<n;i++)
-{
-    for(l=0;l<=k;l++)
-    {
-        temp_sum=temp_sum+p[l].burst_time;
-    }
-    p[i].completion_time=temp_sum;
-    temp_sum=0;
-    if(k<n)
-    k++;
-}
-for(i=0;i<n;i++)
-{
-p[i].turn_Around_time=p[i].completion_time-p[i].arrival_time;
-p[i].waiting_time=p[i].turn_Around_time-p[i].burst_time;
-}
-printf("Process Number\tArrival Time\tBurst Time\tCompletion Time\tTurnAround Time\tWaiting Time\n");
-for(i=0;i<n;i++)
-{
-printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",i+1,p[i].arrival_time,p[i].burst_time,p[i].completion_time,p[i].turn_Around_time,p[i].waiting_time);
-}
+    
+    p[0].ct=p[0].at+p[0].bt;
+    p[0].tat=p[0].ct-p[0].at;
+    p[0].wt=p[0].tat-p[0].bt;
 
-
-temp_sum=0;
-printf("Average waiting time is :- ");
-for(i=0;i<n;i++)
-{
-    temp_sum=temp_sum+p[i].waiting_time;
-}
-avg_time=(float)temp_sum/n;
-printf("%f\n",avg_time);
-temp_sum=0;
-printf("Average Turn Around time is = ");
-for(i=0;i<n;i++)
-{
-    temp_sum=temp_sum+p[i].turn_Around_time;
-}
-avg_time1=(float)temp_sum/n;
-printf("%f\n",avg_time1);
-
-    // print top bar
-    printf(" ");
-    for(i=0; i<n; i++) {
-        for(j=0; j<p[i].burst_time; j++) printf("--");
-        printf(" ");
+    for(int i=1;i<n;i++){
+        ct=p[i-1].ct;
+        if(p[i].at>ct){
+            p[i].ct=p[i].at+p[i].bt;
+        }
+        else{
+            p[i].ct=ct+p[i].bt;
+        }
+        p[i].tat=p[i].ct-p[i].at;
+        p[i].wt=p[i].tat-p[i].bt;
     }
-    printf("\n|");
- 
-    // printing process id in the middle
-    for(i=0; i<n; i++) {
-        for(j=0; j<p[i].burst_time - 1; j++) printf(" ");
-        printf("P%d", i+1);
-        for(j=0; j<p[i].burst_time - 1; j++) printf(" ");
-        printf("|");
+
+    printf("\nPid\tArrival time\tBurst Time\tCompletion Time\tTurnAround Time\tWait Time\n");
+    for(int i=0;i<n;i++){
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",p[i].pid,p[i].at,p[i].bt,p[i].ct,p[i].tat,p[i].wt);
     }
-    printf("\n ");
-    // printing bottom bar
-    for(i=0; i<n; i++) {
-        for(j=0; j<p[i].burst_time; j++) printf("--");
-        printf(" ");
+
+    printf("\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<p[i].bt;j++){
+            printf("--");
+        }
     }
     printf("\n");
- 
-    // printing the time line
-    printf("0");
-    for(i=0; i<n; i++) {
-        for(j=0; j<p[i].burst_time; j++) printf("  ");
-        if(p[i].turn_Around_time > 9) printf("\b"); // backspace : remove 1 space
-        printf("%d", p[i].completion_time);
- 
+    for(int i=0;i<n;i++){
+        for(int j=0;j<p[i].bt-1;j++){
+            printf("  ");
+        }
+        printf("P%d |",p[i].pid);
     }
-printf("\n");
+    printf("\n");
+   printf("\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<p[i].bt-1;j++){
+            printf("  ");
+        }
+        printf(" %d  ",p[i].ct);
+    }
+    printf("\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<p[i].bt;j++){
+            printf("--");
+        }
+    }
+    printf("\n");
+
 }
